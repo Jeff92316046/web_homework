ad522d7bfaa7ff420956e6341554e242f6ef34e5
JEFF Liao <jeffliao92316046@gmail.com>
2023-10-26 15:59:34 +0800

    owo

--- 5/main.js
+++ 5/main.js
@@ -1,22 +1,36 @@
-function correct(){
-    alert("correct")
+function correct(n){
+    var question = document.getElementsByClassName('q'+n)
+    question[0].setAttribute('disabled','true')
+    question[1].setAttribute('disabled','true')
+    question[2].setAttribute('src','o.png')
+    question[2].setAttribute('style','width:30;display:block')
     point++
+    answered++
     var point_node = document.getElementById("point")
-    point_node.textContent = "分數：" + point
+    point_node.textContent = "分數：" + point + "/" +answered 
 }
-function incorrect(){
-    alert("wrong!!")
+function incorrect(n){
+    var question = document.getElementsByClassName('q'+n)
+    question[0].setAttribute('disabled','true')
+    question[1].setAttribute('disabled','true')
+    question[2].setAttribute('src','x.png')
+    question[2].setAttribute('style','width:30;display:block')
+    answered++
+    var point_node = document.getElementById("point")
+    point_node.textContent = "分數：" + point + "/" +answered 
 }
 const answer = ["い","ひ","び","く"]
 function answer_check(n){
     var answer_input = document.getElementById("input"+String(n)).value;
     if(answer[Number(n)-1]===answer_input){
-        correct()
+        correct(n+10)
     }else{
-        incorrect()
+        incorrect(n+10)
     }
+    
 }
 let point = 0
+let answered = 0
 let input = prompt("輸入名字")
 
 var newDiv = document.createElement("marquee");
